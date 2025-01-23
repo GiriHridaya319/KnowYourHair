798e5cecdc8d9276795a35511b69d404b018089c
GiriHridaya319 <hridayagiri78@gmail.com>
2025-01-24 00:10:56 +0545

    initial commit

--- base/urls.py
+++ base/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 # home is the function created in views
 urlpatterns = [
-    path('', views.home, name='hair-home'),
-    path('hairfallprediction/', views.hair_fall_prediction, name='hair-hairfallprediction'),
-    path('Blog/', views.blog, name='hair-blog'),
-    path('login/', views.login, name='hair-login'),
-    path('registration/', views.registration, name='hair-registration'),
+    path('', views.home, name='KnowYourHair-home'),
+    path('hairfallprediction/', views.hair_fall_prediction, name='KnowYourHair-hairfallprediction'),
+    path('Blog/', views.blog, name='KnowYourHair-blog'),
+    path('login/', views.login, name='KnowYourHair-login'),
+    path('registration/', views.registration, name='KnowYourHair-registration'),
 ]
