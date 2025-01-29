201d83bb60969cd24c54174aa07fc69223abc89c
GiriHridaya319 <hridayagiri78@gmail.com>
2025-01-29 23:17:24 +0545

    initial commit

--- user/views.py
+++ user/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.hashers import make_password, check_password
-from .models import Agent, Customer
 from django.contrib.auth import logout
+from .models import Agent, Customer
 
 
-# Register View (for both Agent and Customer)
 def register(request):
     if request.method == 'POST':
         user_type = request.POST.get('user_type')
@@ -19,15 +18,12 @@ def register(request):
         address = request.POST.get('address', '')
         age = request.POST.get('age', '')
 
-        # Check if passwords match
         if password != confirm_password:
             messages.error(request, "Passwords do not match!")
             return redirect('registration')
 
-        # Hash the password
         hashed_password = make_password(password)
 
-        # Register Customer or Agent
         if user_type == 'customer':
             customer = Customer(username=username, password=hashed_password, image=image, email=email, age=age, gender=gender, phone_number=phone_number)
             customer.save()
@@ -37,30 +33,27 @@ def register(request):
             agent.save()
             messages.success(request, "Agent registered successfully! Please log in.")
 
-        return redirect('login')  # Redirect to login page after successful registration
+        return redirect('login')
 
     return render(request, 'user/register.html')
 
 
-# Login View for both Agent and Customer
 def login(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
 
-        # Check if the user is an Agent
         try:
             agent = Agent.objects.get(username=username)
             if check_password(password, agent.password):
-                return redirect('KnowYourHair-home')  # Redirect to agent dashboard
+                return redirect('home')
             else:
                 return render(request, 'login.html', {'error': 'Invalid credentials for Agent'})
         except Agent.DoesNotExist:
-            # Check if the user is a Customer
             try:
                 customer = Customer.objects.get(username=username)
                 if check_password(password, customer.password):
-                    return redirect('KnowYourHair-home')  # Redirect to customer dashboard
+                    return redirect('home')
                 else:
                     return render(request, 'login.html', {'error': 'Invalid credentials for Customer'})
             except Customer.DoesNotExist:
@@ -69,7 +62,6 @@ def login(request):
     return render(request, 'user/login.html')
 
 
-# Logout View
 def custom_logout(request):
     logout(request)
-    return redirect('KnowYourHair-login')
+    return redirect('login')
