8612ba63abb469ab543c051d745d82e1cc442cc1
GiriHridaya319 <hridayagiri78@gmail.com>
2025-02-06 21:55:44 +0545

    product page commit

--- product/models.py
+++ product/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth.models import User
+from PIL import Image
 
 
 class Product(models.Model):
@@ -26,3 +27,11 @@ class Product(models.Model):
 
     def get_absolute_url(self):
         return reverse('product-detail', kwargs={'pk': self.pk})
+
+    def save(self):
+        super().save()  # run the save method of the parent class
+        img = Image.open(self.image.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
--- product/urls.py
+++ product/urls.py
@@ -1,19 +1,22 @@
 from django.urls import path
 from django.conf.urls.static import static
-
 from KnowYourHair import settings
-from .views import ProductUpdateView, ProductDeleteView, ProductCreateView, ProductListView,\
-    Product, ProductDetailView   # PostListView is the class created in views
+from .views import (
+    ProductUpdateView,
+    ProductDeleteView,
+    ProductCreateView,
+    ProductListView,
+    ProductDetailView
+)
 
-# home is the function created in views
 urlpatterns = [
+    # Remove 'product/' from the start of these paths since it's already included in main urls.py
     path('', ProductListView.as_view(), name='KnowYourHair-product'),
-    path('product/<int:pk>/delete', ProductDeleteView.as_view(), name='product-delete'),
-    path('product/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
-    path('product/new/', ProductCreateView.as_view(), name='product-create'),
-    path('product/<int:pk>/update', ProductUpdateView.as_view(), name='product-update'),
+    path('<int:pk>/delete/', ProductDeleteView.as_view(), name='product-delete'),
+    path('<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
+    path('new/', ProductCreateView.as_view(), name='product-create'),
+    path('<int:pk>/update/', ProductUpdateView.as_view(), name='product-update'),
 ]
 
-# if the project is in development mode, then add the following line to the urlpatterns
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
